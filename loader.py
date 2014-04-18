55abb6de387ed14842b44c470ac1f91522e7cdc6
Joe <jgreef@caltech.edu>
2014-04-18 14:40:32 -0700

    the code for loading matrices works
    haven't run it to 100% completion, so it might have memory issues.

--- loader.py
+++ loader.py
@@ -1,5 +1,8 @@
 
 from itertools import izip
+from scipy.sparse import coo_matrix
+from scipy.sparse import lil_matrix
+import numpy as np
 
 NUM_USERS = 480189
 NUM_MOVIES = 17770
@@ -7,24 +10,76 @@ NUM_MOVIES = 17770
 base = None
 valid = None
 hidden = None
+probe = None
+qual = None
 
+path = ""
 
-def loadMatrix(setNum, matrix):
-    for dataline, setline in izip(open("all.dta"), open("all.idx")):
+#call this function if your data files are in a different directory
+#from your python files
+def initLoader(file_path):
+    global path
+    path = file_path 
+
+#loads a the set corrosponding to setNum into matrix
+def _loadMatrix(setNum, matrix):
+    i = 0;
+    for dataline, setline in izip(open(path+"all.dta"), open(path+"all.idx")):
+        i += 1
+        if i % 100000 == 0:
+            print i
         if(int(setline) == setNum): 
-            print setline + " " + dataline
+            data = map(int, dataline.split())
+            matrix[data[0], data[1]] = data[3]
+    print "Done"
+            
+def _loadMatrix2(setnum):
+    i = 0;
 
+    for dataline, setline in izip(open(path+"all.dta"), open(path+"all.idx")):
+        i += 1
+        if i % 100000 == 0:
+            print i
+        if(int(setline) == setNum): 
+            data = map(int, dataline.split())
+            matrix[data[0], data[1]] = data[3]
+    print "Done"
 
 def getUserBaseMatrix():
-    x = 1
+    global base
+    if base is None:
+        matrix = lil_matrix((NUM_USERS, NUM_MOVIES), dtype = np.int8)
+        _loadMatrix(1, matrix)
+        base = matrix
+    return base
 def getUserValidMatrix():
-    x = 1
+    global valid
+    if valid is None:
+        matrix = lil_matrix((NUM_USERS, NUM_MOVIES), dtype = np.int8)
+        _loadMatrix(1, matrix)
+        valid = matrix
+    return valid
 def getUserHiddenMatrix():
-    x = 1
+    global hidden 
+    if hidden is None:
+        matrix = lil_matrix((NUM_USERS, NUM_MOVIES), dtype = np.int8)
+        _loadMatrix(1, matrix)
+        hidden = matrix
+    return hidden 
 def getUserProbeMatrix():
-    x = 1
+    global probe
+    if qual is None:
+        matrix = lil_matrix((NUM_USERS, NUM_MOVIES), dtype = np.int8)
+        _loadMatrix(1, matrix)
+        probe = matrix
+    return probe
 def getUserQualMatrix():
-    x = 1
+    global qual 
+    if qual is None:
+        matrix = lil_matrix((NUM_USERS, NUM_MOVIES), dtype = np.int8)
+        _loadMatrix(1, matrix)
+        qual = matrix
+    return qual
 
 
 
@@ -39,5 +94,7 @@ def getMovieProbeMatrix():
 def getMovieQualMatrix():
     x = 1
 
+initLoader("mu/")
 
-loadMatrix(2, 0)
+getUserBaseMatrix()
+
